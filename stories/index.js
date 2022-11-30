a23247a1b628140d4770f9edc886b90ba20d7579
Rahim Jamal <rahim_92@live.ca>
2022-11-30 14:01:51 -0500

    add confirm comp

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,9 @@ import InterviewerList from "../src/components/InterviewerList"
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm"
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -143,6 +146,18 @@ storiesOf("InterviewerList", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", ()=> <Header time="12pm" />);
+  .add("Header", ()=> <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", ()=> <Show 
+  onEdit={action("onEdit")}
+  onDelete={action("onDelete")}
+  student="Lydia Miller-Jones"
+  interviewer = {interviewer}
+  />)
+  .add("Confirm", ()=> <Confirm 
+  onCancel={action("onCancel")}
+  onConfirm={action("onConfirm")}
+  message="Delete the appointment?"
+  />)
 
   
