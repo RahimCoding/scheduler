bfc93c0d46aa54973c28a710e5ac45153aa1b447
Rahim Jamal <rahim_92@live.ca>
2022-12-08 15:59:26 -0500

    lint document

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 
@@ -95,7 +95,7 @@ export default function useApplicationData(initial) {
       axios.get('/api/interviewers')
     ]).then((all) => {
 
-      setState({ ...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data })
+      setState((prev)=>  ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
     });
   }, [])
 
